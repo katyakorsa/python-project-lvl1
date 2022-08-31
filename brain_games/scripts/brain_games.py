e8d49121b03a4a98ccac6c252516212299d8dd8a
EKorsakova <keytilin.kors@yandex.ru>
2022-08-31 22:30:10 +0300

    Проект "Игры разума" Задание №3 - Сторонние библиотеки

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
+from brain_games.scripts.cli import welcome_user
+
+
 def main():
     print("poetry run python -m brain_games.scripts.brain_games\n"
           "Welcome to the Brain Games!")
+    welcome_user()
 
 
 if __name__ == "__main__":
